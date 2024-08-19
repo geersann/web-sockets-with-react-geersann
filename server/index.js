e6c562eb5dd05193fba6ca380bb5101c55954bbb
geersann <148795588+geersann@users.noreply.github.com>
2024-08-19 23:09:14 +0000

    add wss module

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
-//Your implementation
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8082 });
+
+wss.on('connection', function connection(ws) {
+  console.log('New client connected');
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.send('Welcome to the WebSocket server!');
+});
 
 // Do not remove this export. wss should be the name of you WebSocket Server instance
 module.exports = wss;
